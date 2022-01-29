50a659adb97be873123aa2e65683e34dd10d8cf3
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-29 12:43:00 -0500

    initial version

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=("GET", "POST"), strict_slashes=False)
+def index():
+    """Function that displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', methods=("GET", "POST"), strict_slashes=False)
+def hbnb():
+    """Function that displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', methods=("GET", "POST"), strict_slashes=False)
+def text(text):
+    """Display “C” followed by the value of the text variable"""
+    return "C {}".format(escape(text.replace("_", " ")))
+
+
+@app.route('/python/<text>', methods=("GET", "POST"), strict_slashes=False)
+def python_text(python_text='is cool'):
+    """Display “Python” followed by the value of the text variable"""
+    return "Python {}".format(escape(python_text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
