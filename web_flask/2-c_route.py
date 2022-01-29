cffa7d06dc2f6063c86b0e68752599fc1cc54bcb
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-29 10:54:32 -0500

    initial version

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=("GET", "POST"), strict_slashes=False)
+def index():
+    """Function that displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', methods=("GET", "POST"), strict_slashes=False)
+def hbnb():
+    """Function that displays 'HBNB'"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
