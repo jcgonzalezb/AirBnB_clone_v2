4c450a8de8fd6e785f4df224477f8e22eddc53c6
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-29 21:43:56 -0500

    context removed

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def number(n):
 @app.route('/number_template/<int:n>', methods=("GET", "POST"), strict_slashes=False)
 def html_if_int(n):
     """Display a HTML page only if n is an integer"""
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html')
 
 
 if __name__ == "__main__":
