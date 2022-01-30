1a7cbb1f7a1ee6c6c7e64c8bb269abc3a6a208e9
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-30 11:46:57 -0500

    close method added

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -67,3 +67,7 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Deserialize JSON file to objects"""
+        self.__session.close()
