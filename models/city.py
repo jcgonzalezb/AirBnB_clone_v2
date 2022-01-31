774e9dc570cfba8ab9c697b7fdfb414ad1a2ae10
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-30 19:03:13 -0500

    relationship modified

--- models/city.py
+++ models/city.py
@@ -16,7 +16,7 @@ class City(BaseModel, Base):
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship("Place", backref="cities",
-                              cascade="all, delete-orphan")
+                              cascade="all, delete, delete-orphan")
     else:
         state_id = ""
         name = ""
