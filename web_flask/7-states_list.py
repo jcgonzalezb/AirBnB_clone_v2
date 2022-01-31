dec7d8c542a9b3110a161340621e2aa7359c22b8
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-30 22:54:24 -0500

    states_list fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,32 @@
-classmethod(function)
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page. H1 tag: “States”
+    UL tag: List of all State objects present in DBStorage sorted
+    by name (A->Z).
+    LI tag: description of one State: <state.id>: <B><state.name></B>.
+    """
+    states = storage.all(State).values()
+    return render_template('7-states_list.py', states=states)
+
+
+@app.teardown_appcontext
+def close_storage(exc):
+    """
+    Remove the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
